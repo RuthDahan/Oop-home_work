9cfcb25d2155314cb7211beaadad9a72549caf7b
RuthDahan <ruthftj@gmail.com>
2025-11-18 08:34:00 +0200

    Correct Code

---
+++ p_5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p_5
+{
+    using System;
+
+    namespace Program
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                int number = 10;
+                long factorial = MyMath.Factorial(number);
+                Console.WriteLine($"{number}!={factorial}");
+            }
+        }
+
+        static class MyMath
+        {
+            public static long Factorial(int number)
+            {
+                long result = 1;
+                for (; number >= 1; number--)
+                {
+                    result *= number;
+                }
+                return result;
+            }
+        }
+    }
+
+}
